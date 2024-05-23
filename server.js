1ad1266954a615001726afd20938b4cd6229e704
hatch2626 <167622396+hatch2626@users.noreply.github.com>
2024-05-23 14:18:07 +0000

    server.js done

--- server.js
+++ server.js
@@ -1,37 +1,37 @@
-//Needed for dotenv
-require("dotenv").config();
+// Needed for dotenv
+require('dotenv').config();
 
-//Needed for Express
-var express = require('express')
-var app = express()
+// Needed for Express
+var express = require('express');
+var app = express();
 
-//Setting where the location of your EJS files are
-app.set('views', '.')
+// Setting where the location of your EJS files are
+app.set('views', '.');
 
-//Needed for EJS
+// Needed for EJS
 app.set('view engine', 'ejs');
 
-//Needed for public directory
-app.use(express.static(_dirname + '/public'));
+// Needed for public directory
+app.use(express.static(__dirname + '/public')); // Use __dirname instead of _dirname
 
-//Needed for parsing form data
+// Needed for parsing form data
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-//root page
-app.get('/', function(req, res) {
-res.render('index');
+// Root page
+app.get('/', function (req, res) {
+  res.render('index');
 });
 
-app.get('/page2', function(req, res) {
-    res.render('page2');
-    });
+app.get('/page2', function (req, res) {
+  res.render('page2');
+});
 
+app.get('/page3', function (req, res) {
+  res.render('page3');
+});
 
-    app.get('/page3', function(req, res) {
-        res.render('page3');
-        });
-
-//Tells the app which port to run on
-App.listen(8080)
-
+// Tells the app which port to run on
+app.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
