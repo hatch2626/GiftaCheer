8a47df9441c1b9e8993606d79a652c81ed875534
hatch2626 <167622396+hatch2626@users.noreply.github.com>
2024-05-23 05:02:39 +0000

    update server js

---
+++ server.js
@@ -0,0 +1,37 @@
+//Needed for dotenv
+require(“dotenv”).config();
+
+//Needed for Express
+var express = require('express')
+var app = express()
+
+//Setting where the location of your EJS files are
+app.set('views', '.')
+
+//Needed for EJS
+app.set('view engine', 'ejs');
+
+//Needed for public directory
+app.use(express.static(_dirname + '/public'));
+
+//Needed for parsing form data
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//root page
+app.get('/index', function(req, res) {
+res.render('index');
+});
+
+app.get('/page2', function(req, res) {
+    res.render('page2');
+    });
+
+
+    app.get('/page3', function(req, res) {
+        res.render('page3');
+        });
+
+//Tells the app which port to run on
+App.listen(8080)
+
