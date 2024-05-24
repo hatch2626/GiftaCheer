77e165dc71e1ada62746654968c888a414101db0
hatch2626 <167622396+hatch2626@users.noreply.github.com>
2024-05-24 15:42:13 +0000

    updated serverjs

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.post('/add-cheer', async (req, res) => {
 });
 
 // Tells the app which port to run on
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(8080, () => {
+  console.log('Server is running on port 8080');
 });
+
